78a020a10f27081750953aaa150c02b4092a0b30
Charles Vardeman <charles.vardeman@gmail.com>
2022-10-11 10:15:03 -0400

    Adding sample ontology to repository

---
+++ build-development.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mergecmd="rdfx"
+files=$(cat tests/modules.txt | awk -F, '{print $2}')
+# shapefiles=$(cat tests/shapes.txt | awk -F, '{print $2}')
+ONTOLOGY=drone
+SHACL=drone.shacl
+
+VERSION=` grep -i versionInfo modules/core/metadata.ttl | sed 's/[^"]*"\([^"]*\).*/\1/'`
+
+if [ -f "./development/ontology/merged.ttl" ]; then
+    rm ./development/merged.ttl
+fi
+
+echo "Merging Ontology"
+$mergecmd merge data $files -f ttl -o ./development
+
+# echo "Merging Shapes"
+# $mergecmd merge $shapefiles -f ttl -o ./development/raw
+#echo "Generating HTML SHACL Shapes Documentation"
+# $pylodecmd ./development/${SHACL}.ttl -o ./development/${SHACL}.html
+
+# echo "Generating HTML Ontology Documentation"
+# docker run -ti --rm   -v `pwd`/development/ontology:/usr/local/widoco/in   -v `pwd`/development/ontology:/usr/local/widoco/out/doc   dgarijo/widoco -ontFile in/merged.ttl -outFolder out -rewriteAll -webVowl 
+# Clean up temp merged file
+#if [ -f "./development/ontology/merged.ttl" ]; then
+#    rm ./development/ontology/merged.ttl
+#fi
