5d59aed04464ef1fe5a43bd5f67fe5dce4bf6fb5
Charles Vardeman <charles.vardeman@gmail.com>
2022-10-29 09:25:35 -0400

    Testing version env

--- tests/build-release.sh
+++ tests/build-release.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"/.. &> /dev/null && pwd )
+
 mergecmd="rdfx"
-files=$(cat tests/modules.txt | awk -F, '{print $2}')
-# shapefiles=$(cat tests/shapes.txt | awk -F, '{print $2}')
-ONTOLOGY=drone
-SHACL=drone.shacl
+files=$(cat $ROOT_DIR/tests/modules.txt | awk -F, '{print $2}')
 
-VERSION=` grep -i versionInfo modules/core/metadata.ttl | sed 's/[^"]*"\([^"]*\).*/\1/'`
 
-if [ -f "./development/merged.ttl" ]; then
-    rm ./development/merged.ttl
+export VERSION=` grep -i versionInfo modules/core/metadata.ttl | sed 's/[^"]*"\([^"]*\).*/\1/'`
+
+mkdir /tmp/ontology
+
+if [ -f "/tmp/merged.ttl" ]; then
+    rm /tmp/ontology/merged.ttl
 fi
 
 echo "Merging Ontology"
-$mergecmd merge data $files -f ttl -o ./development
+$mergecmd merge data $files -f ttl -o /tmp/ontology
 
-# echo "Merging Shapes"
-# $mergecmd merge $shapefiles -f ttl -o ./development/raw
-#echo "Generating HTML SHACL Shapes Documentation"
-# $pylodecmd ./development/${SHACL}.ttl -o ./development/${SHACL}.html
-
-# echo "Generating HTML Ontology Documentation"
-# docker run -ti --rm   -v `pwd`/development/ontology:/usr/local/widoco/in   -v `pwd`/development/ontology:/usr/local/widoco/out/doc   dgarijo/widoco -ontFile in/merged.ttl -outFolder out -rewriteAll -webVowl 
-# Clean up temp merged file
-#if [ -f "./development/ontology/merged.ttl" ]; then
-#    rm ./development/ontology/merged.ttl
-#fi
+# Build optional shapes if shapes.txt has content
+if [ -s $ROOT_DIR/tests/shapes.txt ]; then
+    echo "Merging Shapes"
+    mkdir /tmp/shapes
+    shapefiles=$(cat $ROOT_DIR/tests/shapes.txt | awk -F, '{print $2}')
+    $mergecmd merge $shapefiles -f ttl -o /tmp/shapes
+fi
